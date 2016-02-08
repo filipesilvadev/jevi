0400cb764acfe16f7a123a90982cf631bccf1fe9
Filipe Silva <filipesilva.dev@gmail.com>
2016-02-07 22:11:31 -0200

    inicio da seção 'Peças'

--- js/jevi_script.js
+++ js/jevi_script.js
@@ -1,3 +1,4 @@
+$(document).ready(function(){
   WebFontConfig = {
     google: { families: [ 'Montserrat:400,700:latin', 'Open+Sans:400,300,300italic,400italic:latin']}
   };
@@ -10,3 +11,27 @@
     var s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(wf, s);
   })();
+
+  (function sliderJevi(){
+    var $overview = $('#slide .overview'),
+      $viewport = $('#slide .viewPort'),
+      $prevBtn = $('#slideController .prev'),
+      $nextBtn = $('#slideController .next');
+
+      var min = 1;
+      var max = $viewport.length;
+
+    $nextBtn.click(function(){
+        if (min+1 > max) return;
+        min++;
+        $($overview).animate({left: "-=100.3%"},1250);
+    });
+
+    $prevBtn.click(function(){
+        if (min-1 < 1) return;
+        min--; 
+        $($overview).animate({left: "+=100.3%"},1250);
+    });
+  })();
+
+});
