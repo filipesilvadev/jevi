91e7540bf056970fb44aab00a12ac8ca64bd1f4b
Filipe Silva <filipesilva.dev@gmail.com>
2016-02-10 02:03:18 -0200

    front da seção 'Agenda'

--- js/jevi_script.js
+++ js/jevi_script.js
@@ -2,8 +2,9 @@ $(document).ready(function(){
   WebFontConfig = {
     google: { families: [
       'Montserrat:400,700:latin',
-      'Open+Sans:400,300,300italic,400italic:latin',
-      'Trade+Winds::latin'
+      'Open+Sans:400,300:latin',
+      'Trade+Winds::latin',
+      'Graduate::latin'
       ]}
   };
   (function() {
@@ -36,6 +37,92 @@ $(document).ready(function(){
         min--; 
         $($overview).animate({left: "+=100.3%"},1000);
     });
+  })();
+
+  (function Agenda(){     
+    
+      var shows = [
+          {
+            show: 'O Inimigo',
+            day: '20',
+            month: 'Fev',
+            hour: '13:30',
+            location: 'Igreja Templo das Águias',
+            city: 'Curitiba-PR'
+          },
+          {
+            show: 'Principados & Potestades',
+            day: '01',
+            month: 'Mar',
+            hour: '13:30',
+            location: 'Igreja Templo das Águias',
+            city: 'Curitiba-PR'
+          },
+          {
+            show: 'Mercador das Mascaras',
+            day: '15',
+            month: 'Abr',
+            hour: '13:30',
+            location: 'Igreja Templo das Águias',
+            city: 'Curitiba-PR'
+          }
+      ];
+
+        for (var i = 0; i < shows.length; i++) {
+          var id = "#prod_"+[i],
+          $article = $(
+            '<article class="closed" id="prod_'+[i]+'">'+
+              '<span class="day"></span>' +
+              '<p class="month"></p>' +  
+              '<p class="show"></p>' +
+              '<div class="event">' +
+              '<p class="hour"></p>'+
+              '<p class="location"></p>'+
+              '<p class="city-state"></p>'+
+              '</div>'+
+              '<button id="btn'+[i]+'">Ver mais</button>'+
+            '</article>'
+          ),
+          $diary = $('#diary .wrapper');        
+          $diary.append($article);
+
+        var month     = $(id + ' .month'),
+            day       = $(id + ' .day'),
+            show      = $(id + ' .show'),
+            hour      = $(id + ' .event .hour'),
+            location  = $(id + ' .event .location'),
+            city      = $(id + ' .event .city-state');
+          
+          month.html(shows[i].month);
+          day.html(shows[i].day);
+          show.html(shows[i].show);
+          hour.html(shows[i].hour);
+          location.html(shows[i].location);
+          city.html(shows[i].city);
+
+         var eachButton = $('#prod_'+[i]+' #btn'+[i]);
+         continue
+
+            eachButton.on('click', function(){
+              var eachShow = $('#prod_'+[i]);
+              if (eachShow.hasClass('closed')) {
+                  eachShow.attr('class', 'open');
+              }else{
+                  eachShow.attr('class', 'closed');
+              }
+
+
+            });
+
+        console.log(eachShow);
+        console.log(eachButton);
+
+        };
+
+        
+
+
+    
   })();
 
 });
